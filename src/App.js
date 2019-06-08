190aa82d5a4419adcea8d46409a9a7ce89c30296
Pedro Mass <pedro.j.mass@gmail.com>
2019-06-07 20:23:09 -0400

    Break out NumberModal into it's own component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,5 @@
+import NumberModal from "./components/NumberModal";
 import React, { useState } from "react";
-import Modal from "react-modal";
-
-Modal.setAppElement("#root");
-
-function NumberModal({ title, onValueSelected = () => {} }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
-  const [value, setValue] = useState(0);
-  const handleInput = e => setValue(e.target.value);
-
-  const addValue = () => {
-    onValueSelected(Number(value));
-    closeModal();
-  };
-
-  return (
-    <>
-      <button onClick={openModal}>{title}</button>
-      {/* todo: style this modal */}
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="pedro"
-      >
-        <h1>{title}</h1>
-        <input
-          type="number"
-          name="value"
-          value={value}
-          onChange={handleInput}
-        />
-        <div>
-          <button onClick={closeModal}>Cancel</button>
-          <button onClick={addValue}>Ok</button>
-        </div>
-      </Modal>
-    </>
-  );
-}
 
 const AddButton = props => <NumberModal {...props} title="Add" />;
 
