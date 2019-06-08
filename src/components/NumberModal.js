7a30e20d470c0209d271d6b43823b414ace97017
Pedro Mass <pedro.j.mass@gmail.com>
2019-06-08 18:51:06 -0400

    Switched NumberModal to use semantic-ui

--- src/components/NumberModal.js
+++ src/components/NumberModal.js
@@ -1,41 +1,61 @@
-import React, { useState } from "react";
-import Modal from "react-modal";
+import React, { useState, useRef } from "react";
+import { Button, Header, Modal, Input } from "semantic-ui-react";
+// import Modal from "react-modal";
+// Modal.setAppElement("#root");
 
-Modal.setAppElement("#root");
-
-export default function NumberModal({ title, onValueSelected = () => {} }) {
+export default function NumberModal({
+  title,
+  onValueSelected = () => {},
+  ...props
+}) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
   const [value, setValue] = useState(0);
   const handleInput = e => setValue(e.target.value);
-
   const addValue = () => {
     onValueSelected(Number(value));
     closeModal();
   };
 
+  const inputRef = useRef();
+  const focusInput = () => inputRef.current && inputRef.current.focus();
+
   return (
     <>
-      <button onClick={openModal}>{title}</button>
-      {/* todo: style this modal */}
       <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="pedro"
+        trigger={
+          <Button onClick={openModal} {...props}>
+            {title}
+          </Button>
+        }
+        open={isModalOpen}
+        onClose={closeModal}
+        basic
+        size="small"
+        onMount={focusInput}
       >
-        <h1>{title}</h1>
-        <input
-          type="number"
-          name="value"
-          value={value}
-          onChange={handleInput}
-        />
-        <div>
-          <button onClick={closeModal}>Cancel</button>
-          <button onClick={addValue}>Ok</button>
-        </div>
+        <Header content={title} textAlign="center" />
+        <Modal.Content>
+          <Input
+            type="number"
+            name="value"
+            value={value}
+            onChange={handleInput}
+            fluid
+            ref={inputRef}
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          {/* todo: center these actions */}
+          <Button onClick={closeModal} secondary inverted>
+            Cancel
+          </Button>
+          <Button onClick={addValue} primary inverted>
+            Ok
+          </Button>
+        </Modal.Actions>
       </Modal>
     </>
   );
